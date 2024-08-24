ab3ad73ada16f7eba776c6c0c8f53e8ced3bf4c8
Alison Avelino <alisonavelino3133@gmail.com>
2024-08-24 19:49:32 -0300

    [ET] feat: first commit

---
+++ src/main/java/com/eventostech/eventos_tech_api/EventosTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.eventostech.eventos_tech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosTechApiApplication.class, args);
+	}
+
+}
